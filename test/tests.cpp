45b244efc8ede6442d8026de9f940b816d6b98f2
shkeLL26 <egsh26@gmail.com>
2026-04-06 16:59:54 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -9,3 +9,57 @@ TEST(task1, test1) {
     STATES result = testMachine.getState();
     EXPECT_EQ(WAIT, result);
 }
+
+TEST(task2, test1) {
+	Automata testMachine;
+	testMachine.off();
+	STATES result = testMachine.getState();
+	EXPECT_EQ(OFF, result);
+}
+
+TEST(task3, test1) {
+	Automata testMachine;
+	testMachine.on();
+	testMachine.coin(-10);
+	testMachine.choice(1);
+	STATES result = testMachine.getState();
+	EXPECT_EQ(CHECK, result);
+}
+
+TEST(task3, test2) {
+	Automata testMachine;
+	testMachine.on();
+	testMachine.coin(20);
+	testMachine.choice(2);
+	STATES result = testMachine.getState();
+	EXPECT_EQ(CHECK, result);
+}
+
+TEST(task3, test3) {
+	Automata testMachine;
+	testMachine.on();
+	testMachine.coin(2);
+	testMachine.choice(7);
+	STATES result = testMachine.getState();
+	EXPECT_EQ(WAIT, result);
+}
+
+TEST(task4, test1) {
+	Automata testMachine;
+	testMachine.on();
+	testMachine.coin(20);
+	testMachine.choice(2);
+	testMachine.check();
+	STATES result = testMachine.getState();
+	EXPECT_EQ(WAIT, result);
+}
+
+TEST(task4, test2) {
+	Automata testMachine;
+	testMachine.on();
+	testMachine.coin(1);
+	testMachine.choice(2);
+	testMachine.check();
+	STATES result = testMachine.getState();
+	EXPECT_EQ(WAIT, result);
+}
