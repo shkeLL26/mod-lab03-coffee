da7f73c926404cbc5335af3ce55dda577a3d51f2
shkeLL26 <egsh26@gmail.com>
2026-04-06 16:16:45 +0300

    Add files via upload

---
+++ src/Automata.cpp
@@ -0,0 +1,97 @@
+#include "Automata.h"
+
+Automata::Automata() {
+	cash = 0;
+	menu = {
+		{ "Americana", 10 },
+		{ "Latte", 20 },
+		{ "Chocolate", 30 },
+		{ "Tea", 40 }
+	};
+	state = OFF;
+}
+
+void Automata::on() {
+	if (state == OFF) {
+		state = WAIT;
+		cout << "\nAutomata is waiting...\n";
+		return;
+	}
+}
+
+void Automata::off() {
+	if (state == WAIT) {
+		state = OFF;
+		cout << "\nAutomata turned OFF";
+		return;
+	}
+}
+
+void Automata::coin(double tenge) {
+	cash += abs(tenge);
+	if (state == WAIT || state == ACCEPT) {
+		state = ACCEPT;
+		cout << "\nAutomata gained: " << abs(tenge) << ". Balance: " << cash;
+		return;
+	}
+}
+
+vector<Product> Automata::getMenu() {
+	return menu;
+}
+
+STATES Automata::getState() {
+	return state;
+}
+
+void Automata::choice(unsigned int option) {
+	if (state == ACCEPT) {
+		state = CHECK;
+		cout << "\nYou chose " << menu[option].itemName;
+		currentPrice = menu[option].price;
+		return;
+	}
+}
+
+void Automata::check() {
+	if (currentPrice <= cash) {
+		cout << "\nStarting cooking...";
+		cook();
+	}
+	else {
+		cout << "\nYou don`t have enough tenge, dude";
+		cancel();
+	}
+}
+
+void Automata::cancel() {
+	cash = 0;
+	if (state == ACCEPT || state == CHECK) {
+		state = WAIT;
+		cout << "\nCancellation\nAutomata is waiting...\n";
+		return;
+	}
+}
+
+void Automata::cook() {
+	if (state == CHECK) {
+		state = COOK;
+		this_thread::sleep_for(4s);
+		finish();
+	}
+}
+
+void Automata::finish() {
+	if (state == COOK) {
+		state = WAIT;
+		cout << "\nReady\nAutomata is waiting...\n";
+		return;
+	}
+}
+
+void showMenu(vector<Product> menu) {
+	for (int i = 0; i < menu.size(); i++) {
+		cout << '\n' << i + 1 << ". " << menu[i].itemName << " Price:" << menu[i].price;
+	}
+	cout << '\n';
+}
---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include "Automata.h"
+
+int main() {
+    Automata coffeMachine;
+
+    showMenu(coffeMachine.getMenu());
+
+    coffeMachine.on();
+    coffeMachine.off();
+    coffeMachine.on();
+
+    coffeMachine.coin(5);
+    coffeMachine.coin(5);
+    coffeMachine.choice(1);
+    coffeMachine.check();
+
+    coffeMachine.coin(5);
+    coffeMachine.choice(2);
+    coffeMachine.cancel();
+
+    coffeMachine.coin(10);
+    coffeMachine.choice(0);
+    coffeMachine.check();
+}
