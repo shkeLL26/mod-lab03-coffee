44ebdf1420e653ea3a53bf54080bba7f1a5768c9
shkeLL26 <egsh26@gmail.com>
2026-04-06 16:17:17 +0300

    Add files via upload

---
+++ include/Automata.h
@@ -0,0 +1,45 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <thread>
+#include <chrono>
+
+using namespace std;
+
+enum STATES {
+    OFF,
+    WAIT,
+    ACCEPT,
+    CHECK,
+    COOK
+};
+
+struct Product {
+    string itemName;
+    int price;
+};
+
+class Automata {
+private:
+    double cash;
+    vector<Product> menu;
+    STATES state;
+    double currentPrice;
+
+    void cook();
+    void finish();
+
+public:
+    Automata();
+
+    void on();
+    void off();
+    void coin(double tenge);
+    void choice(unsigned int option);
+    void check();
+    void cancel();
+    vector<Product> getMenu();
+    STATES getState();
+};
+
+void showMenu(vector<Product> menu);
