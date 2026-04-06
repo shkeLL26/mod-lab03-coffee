4301d0430340a6b0bd821f6b9e223f23512c9fb6
shkeLL26 <egsh26@gmail.com>
2026-04-06 16:32:18 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,11 +1,11 @@
-// Copyright 2022 GHA Test Team
+// Copyright 2026 Shkelev Egor
 
 #include <gtest/gtest.h>
 #include "Automata.h"
 
 TEST(task1, test1) {
-	Automata testMachine;
-	testMachine.on();
-	STATES result = testMachine.getState();
-	EXPECT_EQ(WAIT, result);
+    Automata testMachine;
+    testMachine.on();
+    STATES result = testMachine.getState();
+    EXPECT_EQ(WAIT, result);
 }
